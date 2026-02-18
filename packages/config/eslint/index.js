0aa6b5a514a33306c387689aefdc0eb29009848c
sheikhlimon <sheikhlimon404@gmail.com>
2026-02-18 09:13:54 +0600

    feat: add server base setup and db package

---
+++ packages/config/eslint/index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "@typescript-eslint/recommended",
+    "prettier"
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-explicit-any": "warn"
+  },
+  env: {
+    node: true,
+    es2022: true
+  }
+};
